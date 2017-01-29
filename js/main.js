cb5845b5ea759481be20ebb16ca01f86300e1491
santer545 <obram22@gmail.com>
2017-01-29 19:57:20 +0200

    home changes

--- js/main.js
+++ js/main.js
@@ -72,8 +72,10 @@ function getBrowser() {
     return browsrObj;
 }
 var browserYou = getBrowser();
-if (browserYou.platform == 'mobile') { isMobile = true;
-    document.documentElement.classList.add('mobile') } else { document.documentElement.classList.add('dekstop') }
+if (browserYou.platform == 'mobile') {
+    isMobile = true;
+    document.documentElement.classList.add('mobile')
+} else { document.documentElement.classList.add('dekstop') }
 if ((browserYou.browser == 'ie')) { document.documentElement.classList.add('ie'); }
 if ((browserYou.browser == 'ie' && browserYou.versionShort < +'9') || ((browserYou.browser == 'opera' || browserYou.browser == 'operaWebkit') && browserYou.versionShort < +'18') || (browserYou.browser == 'firefox' && browserYou.versionShort < +'30')) {
     alert('Обновите браузер', '')
@@ -578,3 +580,34 @@ function handleMapObjectClick(event) {
         });
     }
 }
+
+
+
+
+
+var $animation_elements = $('.js-animate');
+var $window = $(window);
+
+function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($animation_elements, function() {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+        //check to see if this current container is within viewport
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('in-view');
+        } else {
+            $element.removeClass('in-view');
+        }
+    });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
